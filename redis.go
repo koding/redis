040cc487ab9b0285d936d47b78e00ea4cd66ebc0
Cihangir SAVAS <cihangir@koding.com>
2014-06-01 14:08:23 -0700

    Redis: add Setex function

--- redis.go
+++ redis.go
@@ -175,6 +175,36 @@ func (r *RedisSession) Expire(key string, timeout time.Duration) error {
 	return nil
 }
 
+// Set key to hold the string value and set key to timeout after a given
+// number of seconds. This command is equivalent to executing the following commands:
+// SET mykey value
+// EXPIRE mykey seconds
+// SETEX is atomic, and can be reproduced by using the previous two
+// commands inside an MULTI / EXEC block. It is provided as a faster alternative
+// to the given sequence of operations, because this operation is very common
+// when Redis is used as a cache.
+// An error is returned when seconds is invalid.
+func (r *RedisSession) Setex(key string, timeout time.Duration, item interface{}) error {
+	reply, err := redis.String(
+		r.Do(
+			"SETEX",
+			r.AddPrefix(key),
+			strconv.Itoa(int(timeout.Seconds())),
+			item,
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	if reply != "OK" {
+		return fmt.Errorf("reply string is wrong!: %s", reply)
+
+	}
+
+	return nil
+}
+
 // Exists returns true if key exists or false if not.
 func (r *RedisSession) Exists(key string) bool {
 	// does not have any err message to be checked, it return either 1 or 0
